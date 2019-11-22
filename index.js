23727120eec48b160528eae68281a84b771c44d9
mablds <marcelobragalemos@gmail.com>
2019-11-22 16:45:42 -0300

    a

--- index.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config/config.js')
-const PORT = 8080;
-const app = config.setUp();
-
-app.listen(PORT, () => {
-    console.log(` --------------------------------------------\n|              SERVER ON!                   |\n|      Running on http://localhost:${PORT}     |\n --------------------------------------------`)
-})
-console.log(`Running on http://localhost:${PORT}`);
