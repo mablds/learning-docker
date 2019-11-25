498352dd8310a054b837cb68d4146c526df14d29
mablds <marcelobragalemos@gmail.com>
2019-11-25 20:37:58 -0300

    verification attempt failed

--- controllers/clock/clockIn.js
+++ controllers/clock/clockIn.js
@@ -3,16 +3,8 @@
 module.exports.byId = async(req, res) => {
    // try {
         const consultant = req.params.id;
-        const date = new Date();
-        const hour = await date.getHours();
-        const min = await date.getMinutes();
-        res.status(200).json({
-            "employerId": consultant,
-            "hour": hour,
-            "min": min,
-            "localTimeTest": `${date.toLocaleTimeString()}`,
-            "todayServer": `${date.toLocaleDateString()}`
-        });
+        
+        res.status(200).json({"hehe":"hihi"});
    // } catch {
    //     res.status(500).send('Internal Error')
    // }
