b7420b203036e59d79eec9d74206957adce07377
mablds <marcelobragalemos@gmail.com>
2019-11-25 17:44:12 -0300

    logger middleware added to all routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,12 @@ const router = express.Router();
 //Handlers
 const clockHandler = require('./clock/clockHandler');
 
+//Import general Middlewares
+const logger = require('../middlewares/logger');
+
+//Set general Middlewares
+router.use(logger);
+
 //Router distribution
 router.use('/clockIn', clockHandler.router);
 
