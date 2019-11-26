b74924d008671e949d96e4c1950b70a01196745f
mablds <marcelobragalemos@gmail.com>
2019-11-26 12:06:35 -0300

    ClockIn middleware adjusted

--- middlewares/clock/clockInValidation.js
+++ middlewares/clock/clockInValidation.js
@@ -2,8 +2,8 @@ module.exports = (req, res, next) => {
     if (!req.body.day || !req.body.month || !req.body.year) {
         return res.status(400).send('Informações inválidas');
     } else {
-        const clockInReq = new Date(req.body.year, (req.body.month - 1), req.body.day); //new Date(YYYY, MM, DD) => Month - 1 because date.month is 0-indexed (0-11 / Jan-Dec);
-        const clockServer = new Date();
+        let clockInReq = new Date(req.body.year, (req.body.month - 1), req.body.day); //new Date(YYYY, MM, DD) => Month - 1 because date.month is 0-indexed (0-11 / Jan-Dec);
+        let clockServer = new Date();
         
         if(clockInReq.getDate() === clockServer.getDate() && clockInReq.getMonth() === clockServer.getMonth() && clockInReq.getFullYear() === clockServer.getFullYear()) {
             next();
