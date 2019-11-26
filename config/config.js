7c93323e94f6446124b8b500c0a95027fd07c8f1
mablds <marcelobragalemos@gmail.com>
2019-11-26 16:58:38 -0300

    removing unitilized libs

--- config/config.js
+++ config/config.js
@@ -1,17 +1,12 @@
 const express = require('express');
 
-
 module.exports.setUpServer = () => {
     const router = require('../controllers/index.js')
     const app = express();
-    
-//    app.use(bodyParser.json());
-//    app.use(bodyParser.urlencoded({ extended: true }));
-    
+
     app.use(express.json());
 
     app.use(router);
     
     return app;
 }
-
